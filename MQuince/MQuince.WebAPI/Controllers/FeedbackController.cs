2db43e51fe931e775f75b495ea626079b99ae4c5
Nikola Kolovic <nikola.kolovic@vega.local>
2020-12-01 13:19:45 +0100

    Refactor WebAPI from https to http project

--- MQuince/MQuince.WebAPI/Controllers/FeedbackController.cs
+++ MQuince/MQuince.WebAPI/Controllers/FeedbackController.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using MQuince.Entities;
-using MQuince.Entities.Drug;
-using MQuince.Repository.SQL;
-using MQuince.Repository.SQL.DataProvider;
 using MQuince.Services.Contracts.DTO;
 using MQuince.Services.Contracts.IdentifiableDTO;
 using MQuince.Services.Contracts.Interfaces;
-using MQuince.Services.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MQuince.WebAPI.Controllers
 {
