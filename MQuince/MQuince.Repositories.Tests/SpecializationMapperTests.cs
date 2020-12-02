3f736d5355bcf05d89a8e2ff5bdf89fe46b679d1
Nikola Kolovic <nikola.kolovic@vega.local>
2020-12-02 19:31:06 +0100

    Refactor unit tests for specialization mapper

--- MQuince/MQuince.Repositories.Tests/SpecializationMapperTests.cs
+++ MQuince/MQuince.Repositories.Tests/SpecializationMapperTests.cs
@@ -13,16 +13,11 @@ namespace MQuince.Repositories.Tests
         [Fact]
         public void Map_specialization_persistence_to_specialization_entity()
         {
-            SpecializationPersistence specializationPersistance = new SpecializationPersistence()
-            {
-                Id = Guid.Parse("11ac21e1-1361-4c06-9751-9666ce10d30a"),
-                Name = "Hirurg"
-            };
+            SpecializationPersistence specializationPersistance = this.GetSpecializationPersistanceFirst();
 
             Specialization specialization = SpecializationMapper.MapSpecializationPersistenceToSpecializationEntity(specializationPersistance);
 
-            Assert.Equal(specializationPersistance.Id, specialization.Id);
-            Assert.Equal(specializationPersistance.Name, specialization.Name);
+            Assert.True(this.IsEqualSpecializationPersistanceAndSpecilizationEntity(specializationPersistance, specialization));
         }
 
         [Fact]
@@ -38,35 +33,12 @@ namespace MQuince.Repositories.Tests
         [Fact]
         public void Map_specialization_persistance_collection_to_specialization_entity_collection()
         {
-            List<SpecializationPersistence> specializationPersistences = this.GetListOfSpecialization();
+            List<SpecializationPersistence> specializationPersistences = this.GetListOfSpecificationPersistance();
 
             List<Specialization> listOfSpecialization = SpecializationMapper.MapSpecializationPersistenceCollectionToSpecializationEntityCollection(specializationPersistences).ToList();
 
-            Assert.True(this.CompareSpecializationPersistanceAndSpecializationEntity(listOfSpecialization[0], specializationPersistences[0]));
-            Assert.True(this.CompareSpecializationPersistanceAndSpecializationEntity(listOfSpecialization[1], specializationPersistences[1]));
-        }
-
-        private bool CompareSpecializationPersistanceAndSpecializationEntity(Specialization specialization, SpecializationPersistence specializationPersistence)
-                        => specialization.Id == specializationPersistence.Id
-                           && specialization.Name.Equals(specializationPersistence.Name);
-
-        private List<SpecializationPersistence> GetListOfSpecialization()
-        {
-            SpecializationPersistence specializationPersistanceFirst = new SpecializationPersistence()
-            {
-                Id = Guid.Parse("11ac21e1-1361-4c06-9751-9666ce10d30a"),
-                Name = "Hirurg"
-            };
-            SpecializationPersistence specializationPersistanceSecond = new SpecializationPersistence()
-            {
-                Id = Guid.Parse("c389f917-3eb8-4f1a-a22c-bbc34b137f69"),
-                Name = "Oftamolog"
-            };
-            List<SpecializationPersistence> listOfSpecializationPersistance = new List<SpecializationPersistence>();
-            listOfSpecializationPersistance.Add(specializationPersistanceFirst);
-            listOfSpecializationPersistance.Add(specializationPersistanceSecond);
-
-            return listOfSpecializationPersistance;
+            Assert.True(this.IsEqualSpecializationPersistanceAndSpecilizationEntity(specializationPersistences[0], listOfSpecialization[0]));
+            Assert.True(this.IsEqualSpecializationPersistanceAndSpecilizationEntity(specializationPersistences[1], listOfSpecialization[1]));
         }
 
         [Fact]
@@ -76,6 +48,39 @@ namespace MQuince.Repositories.Tests
 
             Assert.Throws<ArgumentNullException>(()
                     => SpecializationMapper.MapSpecializationPersistenceCollectionToSpecializationEntityCollection(listOfSpecializationPersistance));
+        }
+
+        private SpecializationPersistence GetSpecializationPersistanceFirst()
+                => new SpecializationPersistence()
+                {
+                    Id = Guid.Parse("11ac21e1-1361-4c06-9751-9666ce10d30a"),
+                    Name = "Hirurg"
+                };
+
+        private SpecializationPersistence GetSpecializationPersistanceSecond()
+                => new SpecializationPersistence()
+                {
+                    Id = Guid.Parse("c389f917-3eb8-4f1a-a22c-bbc34b137f69"),
+                    Name = "Oftamolog"
+                };
+
+        private List<SpecializationPersistence> GetListOfSpecificationPersistance()
+        {
+            List<SpecializationPersistence> listOfSpecificationPersistance = new List<SpecializationPersistence>();
+            listOfSpecificationPersistance.Add(this.GetSpecializationPersistanceFirst());
+            listOfSpecificationPersistance.Add(this.GetSpecializationPersistanceSecond());
+            return listOfSpecificationPersistance;
+        }
+
+        private bool IsEqualSpecializationPersistanceAndSpecilizationEntity(SpecializationPersistence specializationPersistence, Specialization specialization)
+        {
+            if (specializationPersistence.Id != specialization.Id)
+                return false;
+
+            if (!specializationPersistence.Name.Equals(specialization.Name))
+                return false;
+
+            return true;
         }
     }
 }
