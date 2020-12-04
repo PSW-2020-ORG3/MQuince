73975306cad2aafb15481498a85dbf2704e6d422
Nikola Kolovic <nikola.kolovic@vega.local>
2020-12-04 19:35:45 +0100

    Modify homepage

--- MQuince/MQuince.WebAPI/clientapp/public/static/homepage.js
+++ MQuince/MQuince.WebAPI/clientapp/public/static/homepage.js
@@ -1,12 +1,7 @@
 ﻿Vue.component("homepage", {
 	data: function () {
 		return {
-			logged: null,
-			error: '',
-			username: '',
-			usernameError: '',
-			password: '',
-			passwordError: ''
+			
 		}
 	},
 	template: ` 
@@ -19,37 +14,7 @@
 `
 	,
 	methods: {
-		checkFormValid: function () {
-			this.error = '';
-			this.usernameError = '';
-			this.passwordError = '';
-
-			if (this.username == "")
-				this.usernameError = 'Korisnicko ime je obavezno polje!';
-			else if (this.password == "")
-				this.passwordError = 'Sifra je obavezno polje!';
-			else {
-				let loginData = { username: this.username, password: this.password };
-
-
-				axios
-					.post('/users/login', JSON.stringify(loginData))
-					.then(response => {
-						if (response.data != "") {
-							//TODO 1: set cookie
-							window.location.href = "/";
-						}
-						else {
-							//TODO 2: napraviti neki lepsi nacin prikaza 
-							this.error = 'Uneti su pogresni podaci ili je korisnik blokiran!';
-						}
-
-
-					});
-
-
-			}
-		}
+		
 	}
 });
 
