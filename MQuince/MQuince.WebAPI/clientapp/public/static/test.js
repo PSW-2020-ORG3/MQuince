7ff108832aa5dc4ecfa145422a85749948a81235
Nikola Kolovic <nikola.kolovic@vega.local>
2020-12-08 17:44:42 +0100

    Add create page and getting specialization

---
+++ MQuince/MQuince.WebAPI/clientapp/public/static/test.js
@@ -0,0 +1,22 @@
+﻿Vue.component("homepage1", {
+	data: function () {
+		return {
+
+		}
+	},
+	template: ` 
+<div>
+	<section>
+		<div class="container">
+			TEST
+		</div>
+  </section>
+</div>
+`
+	,
+	methods: {
+
+	}
+});
+
+
