019fd6cfcdb0a5601f2a3b44dbf7e617ed3e999f
mmilenkovic3 <majamilenkovic98@gmail.com>
2020-12-13 19:46:23 +0100

    Implement get medications specifications frontend

---
+++ MQuince/MQuince.Integration.HospitalApp/Views/assets/js/index.js
@@ -0,0 +1,17 @@
+function addPage()
+{
+    window.location.replace("http://localhost:49544/home/add");
+}
+function addReports()
+{
+    window.location.replace("http://localhost:49544/home/reports");
+}
+function getSpecification()
+{
+    window.location.replace("http://localhost:49544/home/getSpecification");
+}
+function therapy() {
+    window.location.replace("http://localhost:49544/home/therapy");
+}
+
+/*document.location.href=*/
