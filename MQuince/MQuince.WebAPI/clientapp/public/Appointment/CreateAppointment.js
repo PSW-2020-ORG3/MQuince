ca323cfd83316bf354cbf0ae0f5065cd730078c1
Nikola Kolovic <nikola.kolovic@vega.local>
2020-12-17 21:10:00 +0100

    Modify index.html and createAppointment.html

--- MQuince/MQuince.WebAPI/clientapp/public/Appointment/CreateAppointment.js
+++ MQuince/MQuince.WebAPI/clientapp/public/Appointment/CreateAppointment.js
@@ -29,7 +29,7 @@
     template: ` 
 	<div class="container">
             <!-- Contact Section Heading-->
-            <h5 class=" text-center  mb-0 text-uppercase" style="color:#1abc9c; margin-top: 2rem;">Create appointment</h5>
+            <h5 class=" text-center  mb-0 text-uppercase" style="margin-top: 2rem;">Create appointment</h5>
             <!-- Icon Divider-->
             <!-- Contact Section Form-->
     
@@ -42,8 +42,9 @@
     
                          <div v-bind:hidden="!visibleSpecialization" class="control-group">
 
-                            <div class="form-group floating-form-group controls mb-0 pb-2" style="color: #6c757d;opacity: 1 ;">
+                            <div class="form-group floating-form-group controls mb-0 pb-2" style=";opacity: 1 ;">
                                 <br/>
+
                                 <label style="margin-left: 14%;display: block; width: 100%;text-indent: 1%;">Select specialization</label>
                                 <select class="form-control"  style="text-indent: 1%" v-model="selectedSpecialization"> 
                                     <option v-for="(specialization, index) in specializations" class="option" v-bind:value="specialization.id"  :value="specialization.id">{{specialization.entityDTO.name}}</option>
@@ -88,10 +89,10 @@
                         <br />
 
                         <div class="form-group">
-                            <button v-bind:hidden="!visiblePrevious" v-on:click="previous" style="margin-left: 15%; width: 20%;" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Previous</button>
-                            <button v-if="visiblePrevious == false" v-bind:hidden="!visibleNext" style="margin-left: 65%; width: 20%;" v-on:click="next" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Next</button>
-                            <button v-else v-bind:hidden="!visibleNext" style="margin-left: 30%; width: 20%;" v-on:click="next" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Next</button>
-                            <button v-bind:hidden="!visibleAppointment" style="margin-left: 30%; width: 20%;" v-on:click="submit" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Potvrdi</button>
+                            <button v-bind:hidden="!visiblePrevious" v-on:click="previous" style="background: #1977cc;margin-left: 15%; width: 20%;" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Previous</button>
+                            <button v-if="visiblePrevious == false" v-bind:hidden="!visibleNext" style="background: #1977cc;margin-left: 65%; width: 20%;" v-on:click="next" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Next</button>
+                            <button v-else v-bind:hidden="!visibleNext" style="background: #1977cc;margin-left: 30%; width: 20%;" v-on:click="next" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Next</button>
+                            <button v-bind:hidden="!visibleAppointment" style="background: #1977cc;margin-left: 30%; width: 20%;" v-on:click="submit" class="btn btn-primary btn-xl" id="sendMessageButton" type="button">Potvrdi</button>
                         </div>
                     
                        
