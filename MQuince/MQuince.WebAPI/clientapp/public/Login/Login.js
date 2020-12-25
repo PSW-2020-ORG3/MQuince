58bb69524e53f44c6088bcc8de86d33f867a5311
aca24 <acasavic2409@gmail.com>
2020-12-25 16:05:59 +0100

    Front for login user

---
+++ MQuince/MQuince.WebAPI/clientapp/public/Login/Login.js
@@ -0,0 +1,51 @@
+
+var app = new Vue({
+	el: '#login',
+	data: {
+		username: '',
+		password: ''
+	},
+	methods: {
+		submit() {
+			if (this.username != '' || this.password != '') {
+				axios
+					.post("/api/User", {
+						username: this.username,
+						password: this.password
+					}).then(response => {
+						const loggedIn = localStorage.getItem('user');
+						localStorage.setItem('user-token', token);		
+						if(response.status === 200){
+							alert("Successful login!")
+							window.location.href = "../../public/Appointment/Appointment.html";
+							console.log(response.status)
+							
+						} else {
+							alert("Not successful login!")
+							console.log(response.status)
+						}
+					}).catch(function (error) {
+						localStorage.removeItem('user-token')
+						this.serverError = true;
+						if (error.response) {
+							console.log(error.response.status)
+							
+							alert("User don't exist!")
+							
+						} else if (error.request) {
+							console.log(error.request)
+							alert("Error!")
+						}
+					});
+			
+			}else {
+				console.log(this.username);
+				alert("You have to fill in the form!");
+		}
+	}
+	}
+		
+ })
+
+		
+		
--- MQuince/MQuince.WebAPI/clientapp/public/static/app.js
+++ MQuince/MQuince.WebAPI/clientapp/public/static/app.js
@@ -1,11 +1,13 @@
 ﻿const homepage = { template: '<homepage></homepage>' }
 const createAppointment = { template: '<homepage1></homepage1>'}
 const test = { template:"<create-appointment></create-appointment>"}
+
 const router = new VueRouter({
 	routes: [
         { path: '/', component: homepage },
 		{ path: '/createappointment', component: createAppointment },
 		{ path: '/app', component: test }
+		
 	]
 });
 
