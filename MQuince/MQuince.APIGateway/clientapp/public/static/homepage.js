bce98b5378e9dec4b03dc78d3d5d5d82c5afc8ca
NikoLa437 <nikoladskv@hotmail.rs>
2021-01-03 22:57:57 +0100

    Add frontend and routes for feedbacks

---
+++ MQuince/MQuince.APIGateway/clientapp/public/static/homepage.js
@@ -0,0 +1,21 @@
+﻿Vue.component("homepage", {
+	data: function () {
+		return {
+			
+		}
+	},
+	template: ` 
+<div>
+	<section style="max-height:400px; object-fit: fill;" id="hero" class="d-flex align-items-center">
+		<div class="container">
+		</div>
+  </section>
+</div>
+`
+	,
+	methods: {
+		
+	}
+});
+
+
