b7053bef132c11debd7c1f3f7f03eb2045d805c2
jelena-petkovic <jelenaksipypetkovic@gmail.com>
2021-01-07 14:55:44 +0100

    Modify selenium test for publish feedback

---
+++ MQuince/MQuince.WebAPI.Selenium.EndToEnd.Testing/Pages/PublishFeedbackPage.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace MQuince.WebAPI.Selenium.EndToEnd.Testing.Pages
+{
+    public class PublishFeedbackPage
+    {
+        private IWebDriver webDriver;
+
+        public PublishFeedbackPage(IWebDriver _webdriver)
+        {
+            this.webDriver = _webdriver;
+        }
+
+        [Obsolete]
+        public void FindOptionForPublishFeedback()
+        {
+            IWebElement feedback = webDriver.FindElement(By.Id("feedbackOption"));
+            Assert.That(feedback.Displayed, Is.True);
+            feedback.Click();
+        }
+
+        [Obsolete]
+        public void SelectOptionForPublishFeedback()
+        {
+            IWebElement pending = webDriver.FindElement(By.XPath("//select[@name='feedbackOption']/option[text()='Pending']"));
+            Assert.That(pending.Displayed, Is.True);
+            pending.Click();
+            Assert.That(pending.Selected, Is.True);
+        }
+
+        [Obsolete]
+        public void FindFeedbackForPublish()
+        {
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
+            var element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("button[class='btn']")));
+        }
+
+        [Obsolete]
+        public void SubmitFeedbackForPublish()
+        {
+            IWebElement button = webDriver.FindElement(By.CssSelector("button[class='btn']"));
+            Assert.That(button.Displayed, Is.True);
+            button.Click();
+        }
+
+    }
+}
--- MQuince/MQuince.WebAPI.Selenium.EndToEnd.Testing/PublishFeedbackTest.cs
+++ MQuince/MQuince.WebAPI.Selenium.EndToEnd.Testing/PublishFeedbackTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MQuince.WebAPI.Selenium.EndToEnd.Testing.Pages;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -24,21 +25,15 @@ namespace MQuince.WebAPI.Selenium.EndToEnd.Testing
         [Obsolete]
         public void Publish_Feedback()
         {
+            PublishFeedbackPage publishFeedbackPage = new PublishFeedbackPage(webDriver);
+
             webDriver.Navigate().GoToUrl("https://mquince.herokuapp.com/public/Communication/AdminFeedback.html");
 
-            IWebElement feedback = webDriver.FindElement(By.Id("feedbackOption"));
-            feedback.Click();
-
-            IWebElement pending = webDriver.FindElement(By.XPath("//select[@name='feedbackOption']/option[text()='Pending']"));
-            Assert.That(pending.Displayed, Is.True);
-            pending.Click();
-            Assert.That(pending.Selected, Is.True);
-            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(5));
-            var element = wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("button[class='btn']")));
-            IWebElement button = webDriver.FindElement(By.CssSelector("button[class='btn']"));
-            Assert.That(button.Displayed, Is.True);
-            button.Click();
-            try
+            publishFeedbackPage.FindOptionForPublishFeedback();
+            publishFeedbackPage.SelectOptionForPublishFeedback();
+            publishFeedbackPage.FindFeedbackForPublish();
+            publishFeedbackPage.SubmitFeedbackForPublish();
+            /*try
             {
                 new WebDriverWait(webDriver, TimeSpan.FromSeconds(2)).Until(ExpectedConditions.AlertIsPresent());
                 IAlert alert = webDriver.SwitchTo().Alert();
@@ -47,7 +42,7 @@ namespace MQuince.WebAPI.Selenium.EndToEnd.Testing
             catch (Exception e)
             {
                 //exception handling
-            }
+            }*/
         }
     }
 }
