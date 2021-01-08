005d35f17182ca267e814ac7e9c7b8c945f3c19b
mmilenkovic3 <mmilenkovic3@uns.ac.rs>
2021-01-08 22:15:13 +0100

    Add entities for pharmacy offers

---
+++ MQuince/MQuince.Integration.Repository.MySQL/DataProvider/Util/PharmacyOffersMapper.cs
@@ -0,0 +1,68 @@
+﻿using MQuince.Integration.Entities;
+using MQuince.Integration.Repository.MySQL.PersistenceEntities;
+using MQuince.Integration.Services.Constracts.DTO;
+using MQuince.Integration.Services.Constracts.IdentifiableDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MQuince.Integration.Repository.MySQL.DataProvider.Util
+{
+    public class PharmacyOffersMapper
+    {
+        public static PharmacyOffers MapPharmacyOffersPersistenceToPharmacyOffersEntity(PharmacyOffersPersistance pharmacyOffersPersistance)
+        {
+            if (pharmacyOffersPersistance == null) throw new ArgumentNullException();
+
+            return new PharmacyOffers(pharmacyOffersPersistance.IdOffer,
+                                      pharmacyOffersPersistance.IdTender,
+                                      pharmacyOffersPersistance.PharmacyName,
+                                      pharmacyOffersPersistance.PharmacyEmail,
+                                      pharmacyOffersPersistance.Medicationes,
+                                      pharmacyOffersPersistance.Quantity,
+                                      pharmacyOffersPersistance.Price
+                                      );
+
+        }
+
+        public static PharmacyOffersPersistance MapPharmacyOffersEntityToPharmacyOffersPersistance(PharmacyOffers pharmacyOffers)
+        {
+            if (pharmacyOffers == null) return null;
+
+            PharmacyOffersPersistance retVal = new PharmacyOffersPersistance()
+            {
+                IdOffer = pharmacyOffers.IDOffer,
+                IdTender = pharmacyOffers.IdTender,
+                PharmacyName = pharmacyOffers.PharmacyName,
+                PharmacyEmail = pharmacyOffers.PharmacyEmail,
+                Medicationes = pharmacyOffers.Medicationes,
+                Quantity = pharmacyOffers.Quantity,
+                Price = pharmacyOffers.Price
+            };
+            return retVal;
+        }
+        public static IdentifiableDTO<PharmacyOffersDTO> MapPharmacyOffersEntityToPharmacyOffersIdentifierDTO(PharmacyOffers pharmacyOffers)
+                => pharmacyOffers == null ? throw new ArgumentNullException()
+                                            : new IdentifiableDTO<PharmacyOffersDTO>()
+                                            {
+                                                Key = pharmacyOffers.IDOffer,
+                                                
+                                                //IDTender = pharmacyOffers.IdTender,
+                                                EntityDTO = new PharmacyOffersDTO()
+                                                {
+                                                    TenderID = pharmacyOffers.IdTender,
+                                                    PharmacyName = pharmacyOffers.PharmacyName,
+                                                    PharmacyEmail = pharmacyOffers.PharmacyEmail,
+                                                    Medicationes = pharmacyOffers.Medicationes,
+                                                    Quantity = pharmacyOffers.Quantity,
+                                                    Price = pharmacyOffers.Price,
+
+                                                }
+                                            };
+
+        public static IEnumerable<PharmacyOffers> MapPharmacyOffersPersistanceCollectionToPharmacyOffersEntityCollection(IEnumerable<PharmacyOffersPersistance> clients)
+           => clients.Select(c => MapPharmacyOffersPersistenceToPharmacyOffersEntity(c));
+
+    }
+}
---
+++ MQuince/MQuince.Integration.Services.Constracts/DTO/PharmacyOffersDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MQuince.Integration.Services.Constracts.DTO
+{
+    public class PharmacyOffersDTO
+    {
+        public Guid TenderID { get; set; }
+        public string PharmacyName { get; set; }
+        public string PharmacyEmail { get; set; }
+        public string Medicationes { get; set; }
+        public string Quantity { get; set; }
+        public string Price { get; set; }
+        public PharmacyOffersDTO() { }
+    }
+}
