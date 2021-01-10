b1b9249f8fe547e6624415d43b7d9f9decaae44a
verica123 <verica98mucibabic@gmail.com>
2021-01-10 15:49:38 +0100

    Implement request for director frontend

---
+++ MQuince/MQuince.Integration.HospitalApp/Views/assets/js/requestForDirector.js
@@ -0,0 +1,29 @@
+﻿const uri = 'http://localhost:49544/api/Grpc'
+function sendName() {
+    const name = document.getElementById('name');
+    const quantity = document.getElementById('quantity');
+
+
+    const item = {
+        name: name.value,
+        quantity: quantity.value
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(response => response.json())
+        .then(() => {
+            name.value = '';
+            quantity.value = '';
+
+        })
+        .catch(error => console.error('Unable to add item.', error));
+
+
+} 
