a33249bedda399c7e0659df4f8ef846c14eb7bdf
aca24 <acasavic2409@gmail.com>
2021-01-10 23:30:04 +0100

    Add selenium test for cancel appointment

---
+++ MQuince/MQuince.WebAPI.Selenium.EndToEnd.Testing/CancelAppointmentTest.cs
@@ -0,0 +1,38 @@
+﻿using MQuince.WebAPI.Selenium.EndToEnd.Testing.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MQuince.WebAPI.Selenium.EndToEnd.Testing
+{
+    public class CancelAppointmentTest
+    {
+        private IWebDriver webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            webDriver = new ChromeDriver();
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl("http://localhost:5000/public/index.html");
+        }
+
+        [Test]
+        [Obsolete]
+        public void Cancel_Appointment()
+        {
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(25);
+            LoginPage loginPage = new LoginPage(webDriver);
+            loginPage.Login();
+
+            HomePage homePage = new HomePage(webDriver);
+            homePage.ClickObserveAppointment();
+
+            PatientAppointmentsPage patientAppointmentsPage = new PatientAppointmentsPage(webDriver);
+
+            patientAppointmentsPage.ClickCancelAppointmentButton();
+        }
+
+    }
+}
