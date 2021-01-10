fadf35bfe6b85665a799fcfd1139ac0e409269fc
dusancina98 <dule_srb@hotmail.rs>
2021-01-10 22:01:24 +0100

    Fix integration tests to run on pipeline

--- MQuince/MQuince.StaffManagement.Integration.Testing/SpecializationTests.cs
+++ MQuince/MQuince.StaffManagement.Integration.Testing/SpecializationTests.cs
@@ -25,7 +25,7 @@ namespace MQuince.StaffManagement.Integration.Testing
         {
             HttpClient client = _factory.CreateClient();
             client.DefaultRequestHeaders.Authorization =
-                new AuthenticationHeaderValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJiNzA1NmZjYy00OGZhLTRkZjUtOWU5My0zMzRhYjc1OTVkYWEiLCJyb2xlIjoiUGF0aWVudCIsIm5iZiI6MTYxMDIyMjEzNSwiZXhwIjoxNjEwMzA4NTM1LCJpYXQiOjE2MTAyMjIxMzV9.5yIUtErsOjQNWQGyDdpgaJ8GxZGwImFeJ2vMbn8UEDM");
+                new AuthenticationHeaderValue("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9zaWQiOiJiNzA1NmZjYy00OGZhLTRkZjUtOWU5My0zMzRhYjc1OTVkYWEiLCJyb2xlIjoiUGF0aWVudCIsIm5iZiI6MTYxMDMxMTY3NiwiZXhwIjoxNjQxODQ3Njc2LCJpYXQiOjE2MTAzMTE2NzZ9.3bLgIjzH2hXzPdbRz910Q3Jwk2w-SBNE-WaUVGzk3I8");
 
             HttpResponseMessage response = await client.GetAsync("api/specialization");
 
