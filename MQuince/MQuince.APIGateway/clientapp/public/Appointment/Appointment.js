0408fc23552e550a2dd727d0f20b4c92f4f65bcd
jelena-petkovic <jelenaksipypetkovic@gmail.com>
2021-01-16 15:22:50 +0100

    Modify frontend for appointment reports

--- MQuince/MQuince.APIGateway/clientapp/public/Appointment/Appointment.js
+++ MQuince/MQuince.APIGateway/clientapp/public/Appointment/Appointment.js
@@ -4,7 +4,14 @@
 		appointments: [],
 		doctors: [],
 		reportEntity: null,
-		reportText: ''
+		reportText: '',
+		doctor: null,
+		doctorName: '',
+		dateTime: '',
+		appointment: null,
+		timeFrom: '',
+		timeTo: '',
+		doctorID: null
 	},
 	mounted() {
 		var role = localStorage.getItem('keyRole');
@@ -39,19 +46,44 @@
 	},
 	methods: {
 		report: function (appointmentId) {
-		
 			var modal = document.getElementById("myModal");
 			modal.style.display = "block";	
-
+			
 			axios
 				.get('/gateway/Appointment/GetReportForAppointment', {
 					params: {
 						id: appointmentId
 					}
 				}).then(response => {
+					console.log("report poziv",this.doctorID)
 					this.reportEntity = response.data
 					this.reportText = this.reportEntity.entityDTO.reportText
 				})
+				
+			axios
+				.get('/gateway/Appointment/'+ appointmentId, {
+					headers: {
+                       'Authorization': localStorage.getItem('keyToken')
+                    }
+				}).then(response => {
+					this.doctorID = response.data.entityDTO.doctorId
+					this.dateTime = response.data.entityDTO.endDateTime.substr(0, 10);
+					this.timeFrom = response.data.entityDTO.startDateTime.substr(11, 5);
+					this.timeTo = response.data.entityDTO.endDateTime.substr(11, 5);
+
+					axios
+						.get('/gateway/Doctor/'+ this.doctorID, {
+							headers: {
+							   'Authorization': localStorage.getItem('keyToken')
+							}
+						}).then(response => {
+							console.log("Doc poziv",response.data)
+							this.doctorName = response.data.entityDTO.name + ' ' + response.data.entityDTO.surname;
+						})
+
+				})
+
+
         
 		},
 		close: function () {
